7ddc37134f1074aebcb70aba974665f8ad7e38b2
Breno Cunha Queiroz <breno1423@gmail.com>
2019-06-25 09:02:20 -0300

    Feat: Basic functions penterSoftware
    Go to home
    Go to coordinate
    Draw point

--- src/PlatformIO/lib/penterSoftware/penterSoftware.cpp
+++ src/PlatformIO/lib/penterSoftware/penterSoftware.cpp
@@ -14,55 +14,121 @@ void PenterSoftware::begin(){
     pinMode(motorStep[i],OUTPUT);
     pinMode(motorDir[i],OUTPUT);
   }
-}
-
-void PenterSoftware::point(float x, float y){
-
+  for (int i = 0; i < 2; i++) {
+    pinMode(stopButton[i],INPUT);
+  }
 }
 
 void PenterSoftware::moveHome(){
+    // Home X axis
+    digitalWrite(motorDir[motorX],HIGH);// Move 1cm positive
+    for(int i = 0; i < abs(int(10*Kx)); i++) {
+      digitalWrite(motorStep[motorX],HIGH);
+      delayMicroseconds(200);
+      digitalWrite(motorStep[motorX],LOW);
+      delayMicroseconds(200);
+    }
+    digitalWrite(motorDir[motorX],LOW);// Move negative until endstop
+    while(digitalRead(stopX)!=0){
+      digitalWrite(motorStep[motorX],HIGH);
+      delayMicroseconds(200);
+      digitalWrite(motorStep[motorX],LOW);
+      delayMicroseconds(200);
+    }
+    digitalWrite(motorDir[motorX],HIGH);// Move 1cm positive
+    for(int i = 0; i < abs(int(10*Kx)); i++) {
+      digitalWrite(motorStep[motorX],HIGH);
+      delayMicroseconds(400);
+      digitalWrite(motorStep[motorX],LOW);
+      delayMicroseconds(400);
+    }
+    digitalWrite(motorDir[motorX],LOW);// Move negative until endstop (slower)
+    while(digitalRead(stopX)!=0){
+      digitalWrite(motorStep[motorX],HIGH);
+      delayMicroseconds(400);
+      digitalWrite(motorStep[motorX],LOW);
+      delayMicroseconds(400);
+    }
+    currX=0;
 
+    // Home Y axis
+    digitalWrite(motorDir[motorY],HIGH);// Move 1cm positive
+    for(int i = 0; i < abs(int(10*Ky)); i++) {
+      digitalWrite(motorStep[motorY],HIGH);
+      delayMicroseconds(200);
+      digitalWrite(motorStep[motorY],LOW);
+      delayMicroseconds(200);
+    }
+    digitalWrite(motorDir[motorY],LOW);// Move negative until endstop
+    while(digitalRead(stopY)!=0){
+      digitalWrite(motorStep[motorY],HIGH);
+      delayMicroseconds(200);
+      digitalWrite(motorStep[motorY],LOW);
+      delayMicroseconds(200);
+    }
+    digitalWrite(motorDir[motorY],HIGH);// Move 1cm positive
+    for(int i = 0; i < abs(int(10*Ky)); i++) {
+      digitalWrite(motorStep[motorY],HIGH);
+      delayMicroseconds(400);
+      digitalWrite(motorStep[motorY],LOW);
+      delayMicroseconds(400);
+    }
+    digitalWrite(motorDir[motorY],LOW);// Move negative until endstop (slower)
+    while(digitalRead(stopY)!=0){
+      digitalWrite(motorStep[motorY],HIGH);
+      delayMicroseconds(400);
+      digitalWrite(motorStep[motorY],LOW);
+      delayMicroseconds(400);
+    }
+    currY=0;
 }
 
 void PenterSoftware::moveAxes(float XMillimeters,float YMillimeters){
-//Serial.print(XMillimeters);Serial.print(" ");Serial.print(Kx);Serial.print(" ");Serial.print(XMillimeters*Kx);Serial.print(" ");Serial.print(int(XMillimeters*Kx));
-  if(XMillimeters>=0){
-    digitalWrite(motorDir[motorX],HIGH);// Changes the rotation direction
-    for(int i = 0; i < abs(int(XMillimeters*Kx)); i++) {
+  //Serial.print(XMillimeters);Serial.print(" ");Serial.print(Kx);Serial.print(" ");Serial.print(XMillimeters*Kx);Serial.print(" ");Serial.print(int(XMillimeters*Kx));
+    if(currX+XMillimeters>maxX){
+      XMillimeters = maxX-currX;
+    }
+    if(XMillimeters>=0){
+      digitalWrite(motorDir[motorX],HIGH);// Changes the rotation direction
+      for(int i = 0; i < abs(int(XMillimeters*Kx)); i++) {
 
-      digitalWrite(motorStep[motorX],HIGH);
-      delayMicroseconds(300);
-      digitalWrite(motorStep[motorX],LOW);
-      delayMicroseconds(300);
+        digitalWrite(motorStep[motorX],HIGH);
+        delayMicroseconds(300);
+        digitalWrite(motorStep[motorX],LOW);
+        delayMicroseconds(300);
+      }
+    }else{
+      digitalWrite(motorDir[motorX],LOW);// Changes the rotation direction
+      for(int i = 0; i < -int((XMillimeters*Kx)); i++) {
+        digitalWrite(motorStep[motorX],HIGH);
+        delayMicroseconds(300);
+        digitalWrite(motorStep[motorX],LOW);
+        delayMicroseconds(300);
+      }
     }
-  }else{
-    digitalWrite(motorDir[motorX],LOW);// Changes the rotation direction
-    for(int i = 0; i < -int((XMillimeters*Kx)); i++) {
-      digitalWrite(motorStep[motorX],HIGH);
-      delayMicroseconds(300);
-      digitalWrite(motorStep[motorX],LOW);
-      delayMicroseconds(300);
+    currX+=XMillimeters;
+    if(currY+YMillimeters>maxY){
+      YMillimeters = maxY-currY;
     }
-  }
+    if(YMillimeters>=0){
+      digitalWrite(motorDir[motorY],HIGH);// Changes the rotation direction
+      for(int i = 0; i < abs(int(YMillimeters*Ky)); i++) {
 
-  if(YMillimeters>=0){
-    digitalWrite(motorDir[motorY],HIGH);// Changes the rotation direction
-    for(int i = 0; i < abs(int(YMillimeters*Ky)); i++) {
-
-      digitalWrite(motorStep[motorY],HIGH);
-      delayMicroseconds(300);
-      digitalWrite(motorStep[motorY],LOW);
-      delayMicroseconds(300);
+        digitalWrite(motorStep[motorY],HIGH);
+        delayMicroseconds(300);
+        digitalWrite(motorStep[motorY],LOW);
+        delayMicroseconds(300);
+      }
+    }else{
+      digitalWrite(motorDir[motorY],LOW);// Changes the rotation direction
+      for(int i = 0; i < -int((XMillimeters*Ky)); i++) {
+        digitalWrite(motorStep[motorY],HIGH);
+        delayMicroseconds(300);
+        digitalWrite(motorStep[motorY],LOW);
+        delayMicroseconds(300);
+      }
     }
-  }else{
-    digitalWrite(motorDir[motorY],LOW);// Changes the rotation direction
-    for(int i = 0; i < -int((XMillimeters*Ky)); i++) {
-      digitalWrite(motorStep[motorY],HIGH);
-      delayMicroseconds(300);
-      digitalWrite(motorStep[motorY],LOW);
-      delayMicroseconds(300);
-    }
-  }
+    currY+=YMillimeters;
 }
 
 void PenterSoftware::penPoint(){
@@ -80,4 +146,15 @@ void PenterSoftware::penPoint(){
       digitalWrite(motorStep[pen],LOW);
       delayMicroseconds(300);
   }
+}
+
+void PenterSoftware::gotoPosition(float x, float y){
+  float deltaX = x-currX;
+  float deltaY = y-currY;
+  moveAxes(deltaX,deltaY);
+}
+
+void PenterSoftware::point(float x, float y){
+  gotoPosition(x, y);
+  penPoint();
 }
--- src/PlatformIO/lib/penterSoftware/penterSoftware.h
+++ src/PlatformIO/lib/penterSoftware/penterSoftware.h
@@ -11,20 +11,26 @@ public:
   void penPoint();// Draw a point in the paper
   void moveHome();// Calibrate the printer
   void moveAxes(float XMillimeters,float YMillimeters);
+  void gotoPosition(float x, float y);// Go to a coordinate
   void point(float x, float y);// Go to the coordinate and make a point
 private:
   float maxX = 150;
   float maxY = 100;
-  float currX;
-  float currY;
-  const int motorStep[3] = {22,19,3};
-  const int motorDir[3] = {23,21,1};
+  float currX = 0;
+  float currY = 0;
+  const int motorStep[3] = {22,19,3};// penMotor, Xmotor, Ymotor
+  const int motorDir[3] = {23,21,1};// penMotor, Xmotor, Ymotor
+  const int stopButton[2] = {19,20};// X and Y
   float Kx=120;
   float Ky=100;
   enum motorName{
     pen = 0,
     motorX = 1,
     motorY = 2
+  };
+  enum stopButtonName{
+    stopX = 0,
+    stopY = 1
   };
 };
 #endif// PENTERSOFTWARE_H
